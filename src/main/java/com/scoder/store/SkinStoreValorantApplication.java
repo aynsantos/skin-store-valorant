d4709971df8534d857c05d0fc01a9fa033e765ed
aynsantos <allyson.santos@live.com>
2023-04-14 16:33:34 -0300

    Project created

---
+++ src/main/java/com/scoder/store/SkinStoreValorantApplication.java
@@ -0,0 +1,13 @@
+package com.scoder.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinStoreValorantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinStoreValorantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scoder/store/SkinStoreValorantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoder.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinStoreValorantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
