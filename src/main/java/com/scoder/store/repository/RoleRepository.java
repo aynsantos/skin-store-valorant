a7329b64f1f9a14de86f2db5570118fd7c59f117
aynsantos <allyson.santos@live.com>
2023-04-15 09:15:45 -0300

    Repository implementation

---
+++ src/main/java/com/scoder/store/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.scoder.store.repository;
+
+import com.scoder.store.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
